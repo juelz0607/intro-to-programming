bbcd69ee7ca5322d47e8b45a9f34f8a5b752e50b
juelz0607 <juelz0607@yahoo.com>
2015-10-06 19:52:08 +0000

    Exercises lesson comit

---
+++ Exercises/exercise_4.rb
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+uumbers << 11
+
+numbers.unshift(0)
+
+puts numbers
---
+++ Exercises/exercise_8.rb
@@ -0,0 +1,7 @@
+# Old syntax
+
+old_hash = {:one => "one", :two => "two", :three => "three"}
+
+# New syntax 
+
+new_hash =  {one: "one", two: "two", three: "three"}
